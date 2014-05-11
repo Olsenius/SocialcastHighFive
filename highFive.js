50bbc5af8b41b10104dc39802853a25c6148143a
Olsenius <andreas@olsenius.com>
2014-05-11 22:21:24 +0200

    Changed too slow to leave hanging

--- highFive.js
+++ highFive.js
@@ -1,50 +1,61 @@
-function highFive(){
-	$(document).find('a.like_toggle.unliked').each( function (index, el){ 
+function highFive() {
+	$(document).find('a.like_toggle.unliked').each(function(index, el) {
 		$(el).text('High five!');
 	});
 
-	$(document).find('a.like_toggle.liked').each( function (index, el){ 
-	    $(el).text('Too slow');
+	$(document).find('a.like_toggle.liked').each(function(index, el) {
+		$(el).text('Leave hanging');
 	});
 
 
-	$(document).find('li.suffix.decorator').each( function (index, el){ 
-	    el.innerHTML ='high fived this!';
+	$(document).find('li.suffix.decorator').each(function(index, el) {
+		el.innerHTML = 'high fived this!';
 	});
 
 
 	$(document).find('span.like_preview')
-		.each( function (index, el){ 
-	    $(el).find('span').each(function (i, e){
-	    	if (e.innerHTML === ' likes this' || e.innerHTML === ' like this'){
-	    		e.innerHTML =' high fived this!';
-	    	}
-	    })
-	});
+		.each(function(index, el) {
+			$(el).find('span').each(function(i, e) {
+				if (e.innerHTML === ' likes this' || e.innerHTML === ' like this') {
+					e.innerHTML = ' high fived this!';
+				}
+			})
+		});
 
-	$(document).find('div.list_modal').find('h2').each(function (index, el){
+	$(document).find('div.list_modal').find('h2').each(function(index, el) {
 		var element = $(el);
-		if (element.text() === 'People who like this comment'){
+		if (element.text() === 'People who like this comment') {
 			element.text('People who high fived this comment');
 		}
-		if (element.text() === 'People who like this message'){
+		if (element.text() === 'People who like this message') {
 			element.text('People who high fived this message');
 		}
 	});
 
-	if (window.location.pathname === '/notifications'){
-		$(document).find('a.unlike.sc-lightest-text').each( function (index, el){ 
-		    $(el).text('Too slow');
+	if (window.location.pathname === '/notifications') {
+		$(document).find('a.unlike.sc-lightest-text').each(function(index, el) {
+			$(el).text('Leave hanging');
 		});
 
-		$(document).find('span.you_like_this').each( function (index, el){ 
-		    el.innerHTML ='You high fived this!';
+		$(document).find('span.you_like_this').each(function(index, el) {
+			el.innerHTML = 'You high fived this!';
 		});
 
-		$(document).find('a.like.sc-action-link').each( function (index, el){ 
+		$(document).find('a.like.sc-action-link').each(function(index, el) {
 			$(el).text('High five!');
 		});
 	};
 }
+
+function injectJs(link) {
+        var scr = document.createElement("script");
+        scr.type="text/javascript";
+        scr.src=link;
+        (document.head || document.body || document.documentElement).appendChild(scr);
+}
+
+injectJs(chrome.extension.getURL("replace.js"));
+
+highFive();
 
 setInterval(function(){highFive()}, 500);
--- replace.js
+++ replace.js
@@ -8,10 +8,10 @@ function replace(obj) {
 					if (property === "if_youd_like_to_edit_your_message" || property === "already_registered" || property === "delete_confirm" || property === "with_admin" || property === "message")
 						continue;
 
-					obj[property] = obj[property].replace("Unliked", "Too slow");
-					obj[property] = obj[property].replace("unliked", "Too slow");
-					obj[property] = obj[property].replace("Unlike", "Too slow");
-					obj[property] = obj[property].replace("unlike", "Too slow");
+					obj[property] = obj[property].replace("Unliked", "Leave hanging");
+					obj[property] = obj[property].replace("unliked", "Leave hanging");
+					obj[property] = obj[property].replace("Unlike", "Leave hanging");
+					obj[property] = obj[property].replace("unlike", "Leave hanging");
 					obj[property] = obj[property].replace("likes", "high fives");
 					obj[property] = obj[property].replace("liked", "high fived");
 					obj[property] = obj[property].replace("like", "high five");
